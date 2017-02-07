84bbb15fa635a4c2d234629b565aa5a71eb7e92a
Vincent Chow <chow_vincent@utexas.edu>
2017-02-07 15:35:59 -0600

    finished project, added README

---
+++ README-IG-Downloader
@@ -0,0 +1,35 @@
+## Instagram Downloader App
+=========
+
+### About
+An application that downloads the most recent 20 images of a user's instagram profile.
+
+### Tools used
+
+1. Python
+2. Instagram API
+
+### Goals for this project
+
+1. Develop a greater understanding of the Python language
+2. Practice using various Python modules and libraries
+3. Gain experience using Python's web services and libraries
+4. Practice working with JSON data and using Python to interpret it
+5. Research and understand Instagram's API and work with API's in general.
+
+### Process
+
+The idea for this project was to create an application where a user could download all of their photos uploaded to their account on Instagram. All they would have to do is simply run the application and all of their photos would be downloaded into a directory named "Photos". However, after looking at Instagram's API, I quickly found out that all applications are first entered into "Sandbox Mode" where users only have access to their 20 most recent images. I was fairly disappointed in this as I wasn't able to complete my goal, but it turned out to be a fun project in the end.
+
+### Lessons learned
+
+1. Reviewed the process of opening, reading, and working with files.
+2. How to interact with the file system with Python's os module such as creating directories, checking for existing directories, and looking at files.
+3. How to make HTTP get requests with Python's request module.
+4. How to load JSON with Python and gather specific data stored in the JSON response.
+5. How to retrieve data from the web.
+
+### Extensions for the future
+
+1. Take the application to the next step and apply to go Live and have full access to Instagram content.
+2. Automate the script where an image is immediately downloaded once the user posts again. 
--- instagram_downloader.py
+++ instagram_downloader.py
@@ -4,11 +4,15 @@ Created on Feb 7, 2017
 @author: chow_vincent
 '''
 
-import os       # Used for file system operations
-import shutil   # Used for overwriting directory if it already exists
+import os                   # Used for file system operations
+import shutil               # Used for overwriting directory if it already exists
+import json                 # Used for loading JSON data
+import urllib.request       # Used for retrieving data from URLs
+import requests             # Used for http GET request
 
 access_token = ""
 def set_user_access_token(path):
+    global access_token
     found = False
     for entry in os.scandir(path):
         if not entry.name.startswith(".") and entry.is_file() and entry.name == "config.txt":
@@ -22,8 +26,7 @@ def set_user_access_token(path):
                             print("Please format your config.txt exactly like...")
                             print("access_token=<your access_token here>\n") 
                             exit("Exiting due to issue finding access token")
-                        else:
-                            print("Access Token found...")
+
     if not found:
         print("No config.txt file found. Please create a file named \"config.txt\" and input your access token.")
         print("Please format your config.txt exactly like...")
@@ -34,25 +37,27 @@ def create_gallery_dir():
         print("Creating \"Photos\" folder...")
         os.mkdir("Photos")
     else:
+        print("Photos directory already exists.")
         answer = input("Do you want to override your Photos folder, Y or N? ")
         if answer == "Y" or answer == "y" or answer == "yes" or answer == "Yes" or answer == "YES":
             shutil.rmtree("Photos")
             print("Creating \"Photos\" folder...")            
             os.mkdir("Photos")
         elif answer == "N" or answer == "n" or answer == "no" or answer == "No" or answer == "NO":
-            exit("Exiting... Please copy over existing files in the Photos folder and try again")
+            exit("Exiting... Please copy over existing files in the Photos folder and try again.")
         else:
             print("Please input Y or N.")
             create_gallery_dir()
         
-        
-        
-        
+
+def main():
+    response = requests.get("https://api.instagram.com/v1/users/self/media/recent?access_token=" + str(access_token))
+    json_data = json.loads(response.text)
+    for index in range(0, len(json_data["data"])):
+        pic_url = json_data["data"][index]["images"]["standard_resolution"]["url"]
+        print("Creating ig_" + str(index) + ".jpg", "in the Photos directory")
+        urllib.request.urlretrieve(pic_url, "Photos/ig_" + str(index) + ".jpg")
             
-                
-                    
-
-
 if __name__ == "__main__":
     print("------------------------------------")    
     print("------------------------------------")
@@ -62,4 +67,5 @@ if __name__ == "__main__":
     print("------------------------------------\n")
     set_user_access_token(".")
     create_gallery_dir()
+    main()
     
